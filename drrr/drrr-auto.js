3db0635a2f2cd33f30b6aadb99b7965bbba434a5
icqt <247640408@qq.com>
2023-05-24 18:22:05 +0800

    Update drrr-auto.js

--- drrr/drrr-auto.js
+++ drrr/drrr-auto.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://drrr.com/room/*
 // @grant       none
-// @version     1.2
+// @version     1.20
 // @author      阿太网络 QQ:121610059
 // @update      2023/05/24 上午11:02:03
 // ==/UserScript==
