e6bc6c7b5b0c95f8f906e32e8d421e9b31fdba33
xiaochen <xiaochen@xiaochen.cn>
2023-05-25 13:39:47 +0800

    Update drrr-auto.js

---
+++ drrr/yiyan.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        文心一言浏览器脚本
+// @namespace   Violentmonkey Scripts
+// @match       https://yiyan.baidu.com/
+// @require     https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.min.js
+// @license     MIT
+// @grant       none
+// @version     1.0
+// @author      QQ:121610059
+// @update      2023-05-25 13:46:56
+// ==/UserScript==
